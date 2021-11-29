b869a18d66cf74de721471cb16259c187fb6976d
ferarcin <93416426+ferarcin@users.noreply.github.com>
2021-11-29 00:17:56 -0500

    Segundo Arreglo

--- src/main/java/com/usa/Ciclo4/TiendaVirtualMongo/TiendaVirtualMongoApplication.java
+++ src/main/java/com/usa/Ciclo4/TiendaVirtualMongo/TiendaVirtualMongoApplication.java
@@ -1,13 +1,29 @@
 package com.usa.Ciclo4.TiendaVirtualMongo;
 
+import com.usa.Ciclo4.TiendaVirtualMongo.interfase.ProductInterfase;
+import com.usa.Ciclo4.TiendaVirtualMongo.interfase.UserInterfase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
-public class TiendaVirtualMongoApplication {
+public class TiendaVirtualMongoApplication implements CommandLineRunner{
+	@Autowired
+	private ProductInterfase productInterfase;
+
+	@Autowired
+	private UserInterfase userInterfase;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaVirtualMongoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception{
+		productInterfase.deleteAll();
+		userInterfase.deleteAll();
+	}
 }
